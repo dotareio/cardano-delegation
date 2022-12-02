2848a0b6916477ec569af5c0bc9dd4fed5ffbb88
Dorien <dorien.pine@gmail.com>
2022-12-02 15:33:41 -0700

    rm file

--- ogmios.ts
+++
@@ -1,7 +0,0 @@
-import { createInteractionContext, InteractionContext } from '@cardano-ogmios/client'
-
-const context : InteractionContext = await createInteractionContext(
-  err => console.error(err),
-  () => console.log("Connection closed."),
-  { connection: { port: 1337 } }
-)
