c6af53eb028183e774cbb351397c6a8e537cc62d
Dorien <dorien.pine@gmail.com>
2022-12-27 23:11:29 -0700

    Add file

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const { experiments } = require('webpack');
+
+module.exports = {
+    entry: './src/index.ts',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    experiments: {
+        asyncWebAssembly: true
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    mode: 'development'
+};
